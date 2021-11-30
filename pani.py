75301e4eaf8495f1d9d34ee63479cae28e4cc120
Lavanya Rishishwar <lavanyarishishwar@users.noreply.github.com>
2021-11-29 23:01:56 -0500

    hastily done but closes #1

--- pani.py
+++ pani.py
@@ -69,24 +69,129 @@ def get_ani(comp_file):
     return ani
 
 
+def get_files(folder1: str, ext: str, distance: bool, folder2: str = None):
+
+    run_list = []
+    ani_mat = {}
+    file_names = {"rows": [], "cols": []}
+    file_bnames = {"rows": [], "cols": []}
+    
+    if folder2 is None:
+        # Matrix run mode
+        file_list = glob.glob(os.path.join(folder1, f"*.{ext}"))
+        if len(file_list) == 0:
+            logging.critical(f"{Colors.FAIL}Error: Can't seem to find any files with extension {ext} in" +
+                             f" the directory {folder1}! Exiting. {Colors.ENDC}")
+            sys.exit(1)
+
+        logging.info(f"Found {len(file_list)} files.")
+        # Init
+        file_names["rows"] = file_list
+        file_names["cols"] = file_list
+        for ref_i in range(len(file_list)):
+            ani_mat[file_list[ref_i]] = {}
+            if distance:
+                ani_mat[file_list[ref_i]][file_list[ref_i]] = 0
+            else:
+                ani_mat[file_list[ref_i]][file_list[ref_i]] = 100
+            this_name = os.path.splitext(os.path.basename(file_list[ref_i]))[0]
+            
+            file_bnames["rows"].append(this_name)
+            file_bnames["cols"].append(this_name)
+            
+            for query_i in range(ref_i + 1, len(file_list)):
+                run_list.append([file_list[ref_i], file_list[query_i]])
+
+    else:
+        # Ref run mode
+        ref_list = glob.glob(os.path.join(folder1, f"*.{ext}"))
+        query_list = glob.glob(os.path.join(folder2, f"*.{ext}"))
+        if len(ref_list) == 0:
+            logging.critical(f"{Colors.FAIL}Error: Can't seem to find any reference files with extension {ext} in" +
+                             f" the directory {folder1}! Exiting. {Colors.ENDC}")
+            sys.exit(1)
+
+        if len(query_list) == 0:
+            logging.critical(f"{Colors.FAIL}Error: Can't seem to find any query files with extension {ext} in" +
+                             f" the directory {folder1}! Exiting. {Colors.ENDC}")
+            sys.exit(1)
+
+        logging.info(f"Found {len(ref_list)} ref x {len(query_list)} query files.")
+
+        file_names["rows"] = query_list
+        file_names["cols"] = ref_list
+        for query_i in range(len(query_list)):
+            ani_mat[query_list[query_i]] = {}
+            file_bnames["rows"].append(os.path.splitext(os.path.basename(query_list[query_i]))[0])
+        for ref_i in range(len(ref_list)):
+            file_bnames["cols"].append(os.path.splitext(os.path.basename(ref_list[ref_i]))[0])
+
+        for query_i in range(len(query_list)):
+            for ref_i in range(len(ref_list)):
+                run_list.append([query_list[query_i], ref_list[ref_i]])
+
+    return(run_list, file_bnames, ani_mat, file_names)
+
+
 # Main
 if __name__ == "__main__":
     parser = ArgumentParser(prog=PROGRAM_NAME, add_help=False, description=f'''
                                 {PROGRAM_NAME} - Parallel script for computing pairwise ANIm values.
                                 ''',
                             formatter_class=lambda prog: HelpFormatter(prog, width=120, max_help_position=120))
-    parser.add_argument("-f", "--folder", help="Folder in which input files are located", default="./", required=False,
-                        type=str, dest="folder")
-    parser.add_argument("-o", "--out", help="Output file name", default="ani.tsv", required=False, type=str, dest="out")
-    parser.add_argument("-t", "--threads", help="How many threads to use?", default=10, required=False, type=int,
-                        dest="threads")
-    parser.add_argument("-e", "--ext", help="File extension to look for", default="fasta", required=False, type=str,
-                        dest="ext")
-    parser.add_argument("-d", "--distance", help="Calculate distance instead of similarity", action='store_true')
-    parser.add_argument("-l", "--logfile", help="Log file to save logs in", required=False, type=str,
-                        dest="logfile", default="run.log")
+
     parser.add_argument('--help', '-h', '--h', action='help', help='Show this help message and exit.')
-    args = parser.parse_args()
+
+    subparsers = parser.add_subparsers(title=f"{PROGRAM_NAME} commands")
+    matrix_parser = subparsers.add_parser('matrix', help='All-against-all comparison',
+                                         formatter_class=lambda prog: HelpFormatter(prog, width=120,
+                                                                                    max_help_position=120))
+    matrix_parser.set_defaults(sub_command='matrix')
+    matrix_parser.add_argument("-f", "--folder", help="Folder in which input files are located", default="./", required=False,
+                        type=str, dest="folder")
+    matrix_parser.add_argument("-o", "--out", help="Output file name", default="ani.tsv", required=False, type=str, dest="out")
+    matrix_parser.add_argument("-t", "--threads", help="How many threads to use?", default=10, required=False, type=int,
+                        dest="threads")
+    matrix_parser.add_argument("-e", "--ext", help="File extension to look for", default="fasta", required=False, type=str,
+                        dest="ext")
+    matrix_parser.add_argument("-d", "--distance", help="Calculate distance instead of similarity", action='store_true')
+    matrix_parser.add_argument("-l", "--logfile", help="Log file to save logs in", required=False, type=str,
+                        dest="logfile", default="run.log")
+
+
+    ref_parser = subparsers.add_parser('ref', help='Comparison against reference',
+                                         formatter_class=lambda prog: HelpFormatter(prog, width=120,
+                                                                                    max_help_position=120))
+    ref_parser.set_defaults(sub_command='ref')
+    ref_parser.add_argument("-r", "--ref", help="Folder which contains reference files", default="./", required=False,
+                        type=str, dest="reference")
+    ref_parser.add_argument("-q", "--query", help="Folder which contains query files", default="./", required=False,
+                        type=str, dest="query")
+    ref_parser.add_argument("-o", "--out", help="Output file name", default="ani.tsv", required=False, type=str, dest="out")
+    ref_parser.add_argument("-t", "--threads", help="How many threads to use?", default=10, required=False, type=int,
+                        dest="threads")
+    ref_parser.add_argument("-e", "--ext", help="File extension to look for", default="fasta", required=False, type=str,
+                        dest="ext")
+    ref_parser.add_argument("-d", "--distance", help="Calculate distance instead of similarity", action='store_true')
+    ref_parser.add_argument("-l", "--logfile", help="Log file to save logs in", required=False, type=str,
+                        dest="logfile", default="run.log")
+
+
+    args, unknown_arguments = parser.parse_known_args()
+
+    if len(unknown_arguments) > 0:
+        print(Colors.HEADER)
+        print("User specificed unknown arguments: " + " ".join([str(x) for x in unknown_arguments]))
+        print("Please see the correct usage below:")
+        parser.print_help()
+        print(Colors.ENDC)
+        sys.exit(0)
+
+    if "sub_command" not in args:
+        print(Colors.HEADER)
+        parser.print_help()
+        print(Colors.ENDC)
+        sys.exit(0)
 
     # TODO: Check if directory exist
     # TODO: Check if file list is non-zero
@@ -100,25 +205,10 @@ if __name__ == "__main__":
     console.setFormatter(formatter)
     logging.getLogger().addHandler(console)
 
-    file_list = glob.glob(os.path.join(args.folder, f"*.{args.ext}"))
-    if len(file_list) == 0:
-        logging.critical(f"{Colors.FAIL}Error: Can't seem to find any files with extension {args.ext} in" +
-                         f" the directory {args.folder}! Exiting. {Colors.ENDC}")
-        sys.exit(1)
-
-    run_list = []
-    ani_mat = {}
-    file_bnames = []
-    logging.info(f"Found {len(file_list)} files.")
-    for ref_i in range(len(file_list)):
-        ani_mat[file_list[ref_i]] = {}
-        if args.distance:
-            ani_mat[file_list[ref_i]][file_list[ref_i]] = 0
-        else:
-            ani_mat[file_list[ref_i]][file_list[ref_i]] = 100
-        file_bnames.append(os.path.splitext(os.path.basename(file_list[ref_i]))[0])
-        for query_i in range(ref_i + 1, len(file_list)):
-            run_list.append([file_list[ref_i], file_list[query_i]])
+    if args.sub_command == 'matrix':
+        run_list, file_bnames, ani_mat, file_names = get_files(folder1=args.folder, ext=args.ext, distance=args.distance)
+    elif args.sub_command == 'ref':
+        run_list, file_bnames, ani_mat, file_names = get_files(folder1=args.reference, ext=args.ext, distance=args.distance, folder2=args.query)
 
     logging.info(f"Need to compute {len(run_list)} comparisons.")
     with multiprocessing.Pool(args.threads) as p:
@@ -129,13 +219,14 @@ if __name__ == "__main__":
         if args.distance:
             ani[i] = 100 - ani[i]
         ani_mat[run_list[i][0]][run_list[i][1]] = ani[i]
-        ani_mat[run_list[i][1]][run_list[i][0]] = ani[i]
+        if args.sub_command == 'matrix':
+            ani_mat[run_list[i][1]][run_list[i][0]] = ani[i]
 
     logging.info(f"Printing results to file {args.out}")
     with open(args.out, "w") as f:
-        f.write("\t" + "\t".join(file_bnames) + "\n")
-        for row_i in range(len(file_list)):
-            f.write(file_bnames[row_i])
-            for col_i in range(len(file_list)):
-                f.write(f"\t{ani_mat[file_list[row_i]][file_list[col_i]]}")
+        f.write("\t" + "\t".join(file_bnames["cols"]) + "\n")
+        for row_i in range(len(file_bnames["rows"])):
+            f.write(file_bnames["rows"][row_i])
+            for col_i in range(len(file_bnames["cols"])):
+                f.write(f"\t{ani_mat[file_names['rows'][row_i]][file_names['cols'][col_i]]}")
             f.write("\n")
